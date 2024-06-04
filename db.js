9f8d376fe20d203819e2f627f93b13deb488eee6
Deepak Kumar <deepaklg02@gmail.com>
2024-06-04 09:55:29 +0530

    cart

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 async function connectToDatabase() {
-  // await mongoose.connect(
-  //   "mongodb+srv://iqlipse22:Uprvmz9ikr@cluster0.xkqjppx.mongodb.net/?retryWrites=true&w=majority",
-  //   {
-  await mongoose.connect("mongodb://localhost:27017/levitation", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(
+    "mongodb+srv://iqlipse22:Uprvmz9ikr@cluster0.xkqjppx.mongodb.net/?retryWrites=true&w=majority",
+    {
+      // await mongoose.connect("mongodb://localhost:27017/levitation", {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+    }
+  );
   console.log("Database connected");
 }
 
